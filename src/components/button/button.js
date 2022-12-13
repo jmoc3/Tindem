0dd3932ed4fa7a8e22838a3284b52e1ea55ea56c
Paola <pgelvezandrea@gmail.com>
2022-12-13 01:23:49 -0500

    agregando modal en primera version jsjajs

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -5,6 +5,15 @@ export const buttonCreator=()=>{
 
     const btn = document.createElement('button')
     btn.textContent='Perfiles de interes'
+    let modal = document.getElementById("myModal")
+   let body = document.getElementsByTagName("body")
+   btn.onclick = function(){
+      modal.style.display = "block"
+      body.style.position = "static"
+      body.style.height = "80%"
+      body.style.overflow = "hidden"
+   }
+
     return btn
 
 }
